0541aec2944916fb32b71e38743b54427eea3fe6
sumith prabhu <sumithprabhu07@gmail.com>
2025-01-06 01:32:45 +0530

    config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
-/** @type {import('next').NextConfig} */
-import nextra from 'nextra'
+// /** @type {import('next').NextConfig} */
+// import nextra from 'nextra'
  
-const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+// const withNextra = nextra({
+//   theme: 'nextra-theme-docs',
+//   themeConfig: './theme.config.jsx',
 
-})
+// })
 
  
-export default withNextra()
+// export default withNextra()
  
-// If you have other Next.js configurations, you can pass them as the parameter:
-// export default withNextra({ /* other next.js config */ })
+// // If you have other Next.js configurations, you can pass them as the parameter:
+// // export default withNextra({ /* other next.js config */ })
+
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+}
+ 
+export default nextConfig
